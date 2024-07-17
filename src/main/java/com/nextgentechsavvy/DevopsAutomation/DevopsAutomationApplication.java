cc147963103be201bd0becce94394b6c3310f27d
mithunmajumdar <mithun.majumdar@gmail.com>
2024-07-17 14:37:31 +0530

    Base commit

---
+++ src/main/java/com/nextgentechsavvy/DevopsAutomation/DevopsAutomationApplication.java
@@ -0,0 +1,11 @@
+package com.nextgentechsavvy.DevopsAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAutomationApplication {
+    public static void main(String[] args){
+        SpringApplication.run(DevopsAutomationApplication.class,args);
+    }
+}
